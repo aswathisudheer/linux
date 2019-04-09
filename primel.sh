e1187a7825be5313d6c18c2467e64313c98bcf89
aswathisudheer <aswathivsudheer10@gmail.com>
2019-04-09 20:30:02 +0530

    13th commit

---
+++ primel.sh
@@ -0,0 +1,20 @@
+echo "enter limit"
+read a b
+echo "prime numbers are"
+for (( i=$a ; i<=$b ; i++ ))
+do
+f=0
+for (( j=2 ; j<$i ; j++ ))
+do
+if [ `expr $i % $j` -eq 0 ]
+then 
+f=1
+break
+fi
+done
+if [ $f -eq 0 ]
+then 
+echo "$i"
+fi
+done
+
